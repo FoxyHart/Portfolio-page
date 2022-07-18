0043b53c1c7382435869983ff009be9b213c0704
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2022-07-18 09:00:36 -0400

    Attempting IOS fix, adding dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,28 @@ function scrollUp() {
     if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
   }
   window.addEventListener('scroll', scrollUp)
+
+  /*============= Dark Theme =========*/
+  const themeButton = document.getElementById('theme-button')
+  const darkTheme ='dark-theme'
+  const iconTheme = 'uil-brightness'
+
+  const selectedTheme = localStorage.getItem('selected-theme')
+  const selectedIcon = localStorage.getItem('selected-icon')
+
+  const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+  const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-brightness'
+
+  if(selectedTheme) {
+
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-brightness' ? 'add' : 'remove'](iconTheme) 
+  }
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
